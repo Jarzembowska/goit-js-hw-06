5f721ddca3878be8e6c865005dbc1c6b58ad1806
Anna Maria Jarzembowska <jarzembowska.anma@gmail.com>
2023-03-26 07:48:24 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.body;
+const span = document.querySelector('.color');
+const btn = document.querySelector('.change-color');
+span.textContent = '#ffffff';
+
+btn.onclick = function changeColor() {
+  const color = getRandomHexColor();
+  body.setAttribute('style', 'background-color:' + color);
+  span.textContent = color;
+};
