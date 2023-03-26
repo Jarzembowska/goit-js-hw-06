f71633464be6c35cc4fa51350213f7c995ec37e9
Anna Maria Jarzembowska <jarzembowska.anma@gmail.com>
2023-03-26 16:02:40 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -4,47 +4,49 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+function createBoxes(quantity) {
+  //tworzymy pustą tablicę boxes (dla naszych 'div', które będziemy tworzyć quantity razy),
+  const boxes = [];
+  // tworzymy pętle for, która rozpocznie się od 0 do quantity razy zwiększając swoją iteracje o i++ (czyli o 1 więcej), aż do siągnięcia wartości quantity
+
+  for (let i = 0; i < quantity; i++) {
+    //deklarujemy zmienną box - tworzymy element 'div' w dokumencie html
+
+    const box = document.createElement('div');
+
+    //Ustawiamy style div (box) - wysokość i szerokość
+    box.style.width = 30 + i * 10 + 'px';
+    box.style.height = 30 + i * 10 + 'px';
+
+    // ustawiamy w stylach zmienną dla background (tła) getRandomHexColor
+    box.style.background = getRandomHexColor();
+    box.style.margin = '20px';
+
+    // po każdym przejściu pętli wypychamy utworzony box do tablicy boxes
+    boxes.push(box);
+  }
+
+  //zwracamy tablicę utworzonych boxów ('div')
+  return boxes;
+}
+
 const div = document.getElementById('controls');
-console.log(div.childNodes[1]);
 const input = div.childNodes[1];
+
 input.addEventListener('blur', () => {
   input.value;
-  // console.log(number);
 });
 
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
-let width = 30;
-let height = 30;
 
 createBtn.onclick = () => {
-  // console.log(input.value);
-  if (input.value < 101) {
-    for (let i = 0; i < input.value; i += 1) {
-      let newElem = document.createElement('div');
-
-      newElem.setAttribute(
-        'style',
-        'height:' +
-          height +
-          'px' +
-          '; width:' +
-          width +
-          'px ' +
-          ' ;background-color: ' +
-          getRandomHexColor()
-      );
-      boxes.append(newElem);
-      width = width + 10;
-      height = height + 10;
-    }
-    console.log(boxes.childNodes);
+  if (input.value >= 1 && input.value < 101) {
+    document.getElementById('boxes').append(...createBoxes(input.value));
   } else alert('Wprowadz poprawna wartosc od 1 do 100');
 };
-const boxes = document.getElementById('boxes');
 
 destroyBtn.onclick = () => {
   boxes.innerHTML = '';
-  width = 30;
-  height = 30;
+  input.value = '';
 };
