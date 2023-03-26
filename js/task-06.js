52ca20ea6467d4f2fcb7cf54b7a8d50059bfd817
Anna Maria Jarzembowska <jarzembowska.anma@gmail.com>
2023-03-26 07:00:12 +0200

    up

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const textInput = document.getElementById('validation-input');
 
 textInput.addEventListener('blur', () => {
-  if (textInput.ariaValueMax.length === parseInt(textInput.database.length)) {
+  if (textInput.value.length === parseInt(textInput.data.length)) {
     textInput.classList.add('valid');
     textInput.classList.remove('invalid');
   } else {
